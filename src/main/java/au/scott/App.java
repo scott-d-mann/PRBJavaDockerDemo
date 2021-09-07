065b0d7745442649b3daeb0b2d4d128f065a1fb0
scotdmann <scott.mann@me.com>
2021-09-08 00:18:49 +1000

    Java PRB App Demo

---
+++ src/main/java/au/scott/App.java
@@ -0,0 +1,18 @@
+package au.scott;
+
+/**
+ * PRB Example
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(isDivisibleByFive(50));
+
+    }
+
+    public static boolean isDivisibleByFive(int number){
+        return number % 5 == 0;
+    }
+}
